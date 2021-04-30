ef2125ad065c75c6a732e9ef8afa05791f843423
MintAnge <71545653+MintAnge@users.noreply.github.com>
2021-04-30 22:10:31 +0500

    Add files via upload

---
+++ WpfAp/Error.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfAp
+{
+    /// <summary>
+    /// Логика взаимодействия для Error.xaml
+    /// </summary>
+    public partial class Error : Window
+    {
+        public Error()
+        {
+            InitializeComponent();
+        }
+        public void Clouse_Click(object sender, System.EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
